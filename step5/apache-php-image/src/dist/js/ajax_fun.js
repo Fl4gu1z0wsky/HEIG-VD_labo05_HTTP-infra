62dbeed09f8399d4db00cd71eaa7ae9a00183464
Flag <flagui1462@hotmail.com>
2023-01-04 18:08:36 +0100

    ajax js not working

---
+++ step5/apache-php-image/src/dist/js/ajax_fun.js
@@ -0,0 +1,16 @@
+$(function() {
+	console.log("Loading the fighters in the arena");
+	
+	/*function loadFighters() {
+		$.getJSON( "http://localhost/api", function (fighters) {
+			console.log(fighters);
+			var message = "It seems that all the fighters are cowards ...";
+			if ( students.length > 0) {
+				message = `${fighters[0].name} has ${fighters[0].strenght} in strenght and ${fighters[0].life} points of life`;
+			}
+			$(".my_js_test").text(message);
+		});
+	};*/
+});
+
+//loadFighters();
