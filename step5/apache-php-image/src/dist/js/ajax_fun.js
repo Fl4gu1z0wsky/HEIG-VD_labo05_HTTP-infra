a81da01f0067f07a80b8b5a1438576a827caeaa7
Flag <flagui1462@hotmail.com>
2023-01-04 18:23:27 +0100

    tried with fetch

--- step5/apache-php-image/src/dist/js/ajax_fun.js
+++ step5/apache-php-image/src/dist/js/ajax_fun.js
@@ -1,7 +1,7 @@
-$(function() {
+/*$(function() {
 	console.log("Loading the fighters in the arena");
 	
-	/*function loadFighters() {
+	function loadFighters() {
 		$.getJSON( "http://localhost/api", function (fighters) {
 			console.log(fighters);
 			var message = "It seems that all the fighters are cowards ...";
@@ -10,7 +10,16 @@ $(function() {
 			}
 			$(".my_js_test").text(message);
 		});
-	};*/
-});
+	};
+});*/
 
 //loadFighters();
+
+
+function loadFighters() {
+	fetch('/api')
+	.then((response) => response.json())
+	.then((data) => document.getElementById("my_js_test").innerHTML = (data));
+};
+
+loadFighters();
