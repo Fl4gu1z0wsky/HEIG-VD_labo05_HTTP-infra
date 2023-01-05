92d297762eb68e3e56d99bc235e0269099c440a1
Flag <flagui1462@hotmail.com>
2023-01-05 09:17:30 +0100

    Success ajax request for step 5

--- step5/apache-php-image/src/dist/js/ajax_fun.js
+++ step5/apache-php-image/src/dist/js/ajax_fun.js
@@ -1,25 +1,17 @@
-/*$(function() {
+// We fetch our dynamic resources created before
+function fetchFighters() {
 	console.log("Loading the fighters in the arena");
-	
-	function loadFighters() {
-		$.getJSON( "http://localhost/api", function (fighters) {
-			console.log(fighters);
-			var message = "It seems that all the fighters are cowards ...";
-			if ( students.length > 0) {
-				message = `${fighters[0].name} has ${fighters[0].strenght} in strenght and ${fighters[0].life} points of life`;
-			}
-			$(".my_js_test").text(message);
-		});
-	};
-});*/
-
-//loadFighters();
-
-
-function loadFighters() {
-	fetch('/api')
+	fetch('http://localhost/api')
 	.then((response) => response.json())
-	.then((data) => document.getElementById("my_js_test").innerHTML = (data));
+	.then((data) => loadFighters(data));
 };
 
-loadFighters();
+// We write on the paragraph what we want from our ressource
+function loadFighters(fighters) {
+	console.log("Changing the ajax id paragraph");
+	document.getElementById('ajax').innerText = `${fighters[0].name} has ${fighters[0].strenght} in strenght and ${fighters[0].life} points of life`;
+	document.getElementById('ajax2').innerText = `${fighters[1].name} has ${fighters[1].strenght} in strenght and ${fighters[1].life} points of life`;
+};
+
+// To change dynamically the resource
+setInterval(fetchFighters, 2000);
